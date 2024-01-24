278f83150f77c3af3cad7ce31085db804d7dbe10
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-24 13:05:09 +0600

    startured the layout with differential with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
+import Main from "./Layout/Main";
 
 const App = () => {
   return (
     <div>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <Main></Main>
     </div>
   );
 };
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import TaskContainer from "../components/TaskContainer";
+
+
+const Main = () => {
+    return (
+        <div className="container mx-auto">
+           <Header></Header>
+           <HeroSection></HeroSection>
+           <TaskContainer></TaskContainer>
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
